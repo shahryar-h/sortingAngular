03f6402882b154c5604cd4e023153a69c7996311
shahryar haghighi fard <shahryarhaghighifard@shahryars-MacBook-Pro.local>
2021-02-06 17:55:00 -0500

    initializing project

---
+++ src/app/bubble-sort/bubble-sort.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bubble-sort',
+  templateUrl: './bubble-sort.component.html',
+  styleUrls: ['./bubble-sort.component.sass']
+})
+export class BubbleSortComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
